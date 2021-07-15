3479885155795ca67e23e302201e6e3acf85b7c8
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-07-14 21:50:52 -0300

    Finalização

---
+++ Lambda6EX/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Lambda6Ex.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
--- Lambda6EX/Program.cs
+++ Lambda6EX/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Lambda6Ex.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace Lambda6Ex
 {
@@ -6,7 +11,31 @@ namespace Lambda6Ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> products = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            var avg = products.Select(p => p.Price).Average();
+            Console.WriteLine("Average price: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var productsName = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach(Product p in products){
+                Console.WriteLine(p.Name);
+            }
+        
         }
     }
 }
